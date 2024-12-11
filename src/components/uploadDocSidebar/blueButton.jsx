09fcb8cf3cc66bd48072653e686cb59ece3d24a5
Jai-17 <madhukarjai9@gmail.com>
2024-12-11 18:33:04 +0530

    pdftotext

--- src/components/uploadDocSidebar/blueButton.jsx
+++ src/components/uploadDocSidebar/blueButton.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react"
+import pdfToText from "react-pdftotext";
 
 const UploadButton = ({ name, svg }) => {
   const fileInput = useRef();
@@ -9,11 +10,20 @@ const UploadButton = ({ name, svg }) => {
     }
   }
 
+  function extractText(event) {
+    const file = event.target.files[0];
+    pdfToText(file)
+      .then((text) => console.log(text))
+      .catch((error) =>
+        console.error(error, "Failed to extract text from pdf")
+      );
+  }
+
   return (
     <button className="bg-PrimaryBlue px-5 py-1.5 rounded-md flex w-full justify-center items-center gap-2 hover:bg-blue-500 transition-colors" onClick={handleClick}>
         {name}
         <img src={svg} alt="chat" width={18} height={10} />
-        <input type="file" className="hidden" ref={fileInput}  accept=".pdf, .doc, .docx" />
+        <input type="file" className="hidden" ref={fileInput}  accept=".pdf, .doc, .docx" onChange={extractText} />
     </button>
   )
 }
